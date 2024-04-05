83b4ac7d19b7e65ec2343e8084fe5b7e64ace12e
it-a-me <wobbe.carter@gmail.com>
2024-04-05 03:00:18 -0700

    modernized sway config

--- .config/window_manager/autostart.sh
+++ .config/window_manager/autostart.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+CONFIG_DIR="$(realpath "$(dirname $0)")"
 . /etc/os-release
 if test "$ID" = "gentoo"; then
 	/usr/libexec/polkit-gnome-authentication-agent-1 &
+	dbus-update-activation-environment --all &
 fi
+
+$CONFIG_DIR/exec/wallpaper.sh &
+$CONFIG_DIR/scripts/window_managers/sway/timeout.sh &
+waybar > ~/.cache/waybar.log &
+test $(command -v mako) && mako
