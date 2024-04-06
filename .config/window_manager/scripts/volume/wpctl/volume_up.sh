b8af24032b6eec11ee0ae5a908d39ca12884a094
it-a-me <wobbe.carter@gmail.com>
2024-04-06 14:36:24 -0700

    simplify wpctl max volume

--- .config/window_manager/scripts/volume/wpctl/volume_up.sh
+++ .config/window_manager/scripts/volume/wpctl/volume_up.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 . $(dirname "$0")/../settings.sh
-test $($(dirname "$0")/volume.sh) -lt 150 || exit 0
-exec wpctl set-volume @DEFAULT_AUDIO_SINK@ "${VOLUME_STEP}%+"
+exec wpctl set-volume @DEFAULT_AUDIO_SINK@ "${VOLUME_STEP}%+" -l 1.5
