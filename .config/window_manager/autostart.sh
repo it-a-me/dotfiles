0c1fa9c4b8e67b30fc9e37b4e31bba91a03dbbd2
it-a-me <wobbe.carter@gmail.com>
2024-07-13 00:40:53 -0700

    posix sh fixes

--- .config/window_manager/autostart.sh
+++ .config/window_manager/autostart.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-CONFIG_DIR="$(realpath "$(dirname $0)")"
+CONFIG_DIR="$(realpath "$(dirname "$0")")"
 . /etc/os-release
 if test "$ID" = "gentoo"; then
 	/usr/libexec/polkit-gnome-authentication-agent-1 &
 	dbus-update-activation-environment --all &
 fi
 
-$CONFIG_DIR/exec/wallpaper.sh &
-$CONFIG_DIR/scripts/window_managers/sway/timeout.sh &
-if -x ~/.local/bin/waybar; then
+"$CONFIG_DIR"/exec/wallpaper.sh &
+"$CONFIG_DIR"/scripts/window_managers/sway/timeout.sh &
+if test -x ~/.local/bin/waybar; then
 	~/.local/bin/waybar &> ~/.cache/waybar.log &
 elif command -v waybar; then
 	waybar &> ~/.cache/waybar.log
