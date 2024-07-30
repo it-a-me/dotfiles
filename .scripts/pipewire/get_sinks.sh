3246aacbf7f40d4de2174f1ddae66a9f7c9d01f8
it-a-me <wobbe.carter@gmail.com>
2024-07-29 18:41:15 -0700

    pipewire scripts

---
+++ .scripts/pipewire/get_sinks.sh
@@ -0,0 +1,22 @@
+#!/bin/sh 
+source "${HOME}/.scripts/shutil.sh"
+
+deps jq pw-dump
+PW_DUMP="$(pw-dump)"
+DEFAULT_SINK="$(printf "%s" "$PW_DUMP" |
+    jq -r '.[] |
+        select(.props["metadata.name"] == "default").metadata[] |
+        select(.key == "default.audio.sink").value.name'
+    )"
+pw-dump -N | 
+    jq --arg default_sink "$DEFAULT_SINK" '
+        map(
+            select(.info.props["media.class"] == "Audio/Sink") |
+                { 
+                    id: .id,
+                    name: .info.props["node.name"],
+                    description: .info.props["node.description"],
+                    state:.info.state,
+                    default: (.info.props["node.name"] == $default_sink)
+                }
+            )'
