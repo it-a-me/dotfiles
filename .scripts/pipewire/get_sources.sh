770f8295d99158b01b320f1623d0afdf096ec7b3
it-a-me <wobbe.carter@gmail.com>
2024-08-01 14:18:51 -0700

    add pipewire source and sink scripts

--- .scripts/pipewire/get_sources.sh
+++ .scripts/pipewire/get_sources.sh
@@ -3,20 +3,20 @@ source "${HOME}/.scripts/shutil.sh"
 
 deps jq pw-dump
 PW_DUMP="$(pw-dump)"
-DEFAULT_SINK="$(printf "%s" "$PW_DUMP" |
+DEFAULT_SOURCE="$(printf "%s" "$PW_DUMP" |
     jq -r '.[] |
         select(.props["metadata.name"] == "default").metadata[] |
         select(.key == "default.audio.source").value.name'
     )"
 pw-dump -N | 
-    jq --arg default_sink "$DEFAULT_SINK" '
+    jq --arg default_source "$DEFAULT_SOURCE" '
         map(
-            select(.info.props["media.class"] == "Audio/Sink") |
+            select(.info.props["media.class"] == "Audio/Source") |
                 { 
                     id: .id,
                     name: .info.props["node.name"],
                     description: .info.props["node.description"],
                     state:.info.state,
-                    default: (.info.props["node.name"] == $default_sink)
+                    default: (.info.props["node.name"] == $default_source)
                 }
             )'
---
+++ .scripts/pipewire/select_sink.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+. "${HOME}/.scripts/shutil.sh"
+deps fuzzel wpctl
+
+SOURCES="$(~/.scripts/pipewire/get_sinks.sh)"
+SOURCE="$(printf "%s" "$SOURCES" | jq -r '.[] | if .default == true then "* " + .description else .description end' | fuzzel --dmenu)"
+test $? = 0 || exit 1
+printf "%s" "$SOURCE" | grep -qE '^\* ' && exit 2
+
+ID="$(printf "%s" "$SOURCES" | jq -r --arg source "$SOURCE" '.[] | select(.description == $source) | .id')"
+wpctl set-default "$ID"
+
+
+
