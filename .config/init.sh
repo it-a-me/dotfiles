f9471b7553abf65d3443752867469751c1e57c1d
it-a-me <wobbe.carter@gmail.com>
2024-08-14 02:35:56 -0700

    update posix shell startup

--- .config/init.sh
+++ .config/init.sh
@@ -1,1 +1,12 @@
-test $(command -v fish) && exec fish
+#!/bin/sh
+
+update_environment() {
+	set -a
+	TMP="$(mktemp)"
+	/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator > "${TMP}"
+	. "${TMP}"
+	set +a
+}
+
+update_environment
+test "$(command -v fish)" && exec fish
--- .profile
+++ .profile
@@ -1,2 +1,6 @@
 export ENV=$HOME/.config/init.sh
 export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/SSH_AUTH_SOCK"
+
+if test "$(tty)" = '/dev/tty1'; then
+	XDG_CURRENT_DESKTOP=sway sway
+fi
