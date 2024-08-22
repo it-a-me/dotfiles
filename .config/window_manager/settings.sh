9ba26ccc1e15d31839fa72d3d48211dcd0bbaca4
it-a-me <wobbe.carter@gmail.com>
2024-08-21 20:02:31 -0700

    Update default wallpaper

--- .config/window_manager/settings.sh
+++ .config/window_manager/settings.sh
@@ -4,5 +4,5 @@ BRIGHTNESS_STEP=40
 SCREENSHOT_FORMAT="$(date +"$(xdg-user-dir PICTURES)/screenshots/%Y-%m-%d_%H:%M:%S.png")"
 SCREEN_TIMEOUT="500"
 SCREEN_LOCK="360"
-WALLPAPER_PATH="$HOME/Pictures/Wallpapers/new_forest_lake.png"
+WALLPAPER_PATH="$HOME/Pictures/Wallpapers/forest_lake.png"
 WINDOW_MAN_SCRIPTS="$(dirname "$(realpath "$BASH_SOURCE")")"
