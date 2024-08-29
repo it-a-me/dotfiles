24b4f80e43e1c266912060e8cf93bb0b38e2d999
it-a-me <wobbe.carter@gmail.com>
2024-08-29 15:09:31 -0700

    fish: Fix vscodium running on Xwayland

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -4,6 +4,7 @@ alias dotman "git --git-dir $HOME/.dotfiles --work-tree $HOME"
 alias ffmpeg 'ffmpeg -hide_banner'
 alias ffprobe 'ffprobe -hide_banner'
 alias ffplay 'ffplay -hide_banner'
+alias codium 'codium --ozone-platform-hint=auto'
 
 abbr ollama podman exec -it ollama ollama
 abbr userctl systemctl --user
