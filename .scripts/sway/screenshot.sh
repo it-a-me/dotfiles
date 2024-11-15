e0757bf6449b7462044b243890704dd4bf9ad8da
it-a-me <wobbe.carter@gmail.com>
2024-11-15 00:11:17 -0800

    sway: Copy screenshots to clipboard

--- .scripts/sway/screenshot.sh
+++ .scripts/sway/screenshot.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -eu
 . "$HOME"/.scripts/shutil.sh
-deps jq grim
+deps jq grim wl-copy
 OUTPUT="$(swaymsg -t get_outputs --raw | jq -r '.[] | select(.focused).name')"
 NAME="screenshot-$(date +'%F-%T').png"
 SCREENSHOT_PATH="$HOME/Pictures/screenshots/$NAME"
 grim -t png -o "$OUTPUT" "$SCREENSHOT_PATH"
+wl-copy < "$SCREENSHOT_PATH"
 notify-send "Screenshot saved to $SCREENSHOT_PATH"
